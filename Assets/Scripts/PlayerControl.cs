576499c81e65b9d755ae00db41ff9b65f9b31773
denoboi <deniz.kaymakci@hotmail.com>
2021-04-28 14:54:10 +0300

    Unity

---
+++ Assets/Scripts/PlayerControl.cs
@@ -0,0 +1,38 @@
+﻿using System.Collections;
+using System.Collections.Generic;
+using UnityEngine;
+
+public class PlayerControl : MonoBehaviour
+{
+    public float horizontalinput = 10.0f;
+    public float speed = 30.0f;
+
+    public GameObject projectilePrefab;
+
+    // Start is called before the first frame update
+    void Start()
+    {
+        
+    }
+
+    // Update is called once per frame
+    void Update()
+
+    {
+        if (transform.position.x < -15)
+        {
+            transform.position = new Vector3(-15, transform.position.y, transform.position.z);
+        }
+        else if (transform.position.x > 20)
+        {
+            transform.position = new Vector3(20, transform.position.y, transform.position.z);
+        }
+        horizontalinput = Input.GetAxis("Horizontal");
+        transform.Translate(Vector3.right * horizontalinput * Time.deltaTime * speed);
+
+        if (Input.GetButtonDown("Fire1"))
+            Instantiate(projectilePrefab,transform.position, projectilePrefab.transform.rotation);
+
+        
+    }
+}
